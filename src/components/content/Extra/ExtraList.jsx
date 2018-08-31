951897cf37dfd8d0e503800150685802f947d9d9
EDUARDO BENITEZ <eduardobenitez@MacBook-Pro-de-EDUARDO.local>
2018-08-31 16:01:57 -0500

    mensaje de elemento no encontrado

--- src/components/content/Extra/ExtraList.jsx
+++ src/components/content/Extra/ExtraList.jsx
@@ -5,11 +5,17 @@ import { Row, Col } from 'antd';
 export const ExtraList = ({videos}) => (
     <div>
         <Row type="flex" justify="space-around" style={{ marginTop: "4em" }}>
-            {videos.map((i, key) => (
+            { videos &&  videos.length> 0?
+
+                videos.map((i, key) => (
                 <Col md={7} sm={24} xs={24} span={4} key={key} style={{ marginBottom: "2em" }}>
                     <ExtraComponent {...i} key={key} />
                 </Col>
-            ))}
+            )):
+            
+            <div>No se encontraron resultados</div>
+            
+            }
         </Row>
 
     </div>
